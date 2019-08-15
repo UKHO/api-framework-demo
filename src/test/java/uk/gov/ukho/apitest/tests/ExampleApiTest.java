bb71663dd1b438249a30baec6d573f012e1bd160
Greg Zealley <greg.zealley@ukho.gov.uk>
2019-08-15 15:45:45 +0100

    Add tests for POST endpoint

--- src/test/java/uk/gov/ukho/apitest/tests/ExampleApiTest.java
+++ src/test/java/uk/gov/ukho/apitest/tests/ExampleApiTest.java
@@ -1,7 +1,10 @@
 package uk.gov.ukho.apitest.tests;
 
+import com.github.cliftonlabs.json_simple.JsonObject;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.ExtractableResponse;
 import org.apache.http.HttpStatus;
+import org.assertj.core.api.SoftAssertions;
 import org.junit.Before;
 import org.junit.Test;
 import uk.gov.ukho.apitest.services.ExampleJsonApiService;
@@ -20,10 +23,48 @@ public class ExampleApiTest {
 
     @Test
     public void WhenCallPostsEndpointThenSuccessStatusReturned() {
-
         response = exampleApiService.getAllPosts();
 
         assertThat(response.statusCode())
+                .as("status code")
                 .isEqualTo(HttpStatus.SC_OK);
+    }
+
+    @Test
+    public void WhenCallPostsEndpointThenJsonBodyIsCorrect() {
+        response = exampleApiService.getAllPosts();
+
+        JsonPath jsonPath = response.jsonPath();
+
+        SoftAssertions softly = new SoftAssertions();
+
+        softly.assertThat(jsonPath.getString("id"))
+                .as("id content")
+                .contains("1", "2");
+
+        softly.assertThat((int) jsonPath.get("id.size"))
+                .as("id size")
+                .isEqualTo(100);
+
+        softly.assertAll();
+    }
+
+    @Test
+    public void WhenCreateANewPostThenSuccessStatusReturned() {
+        String body = createExampleJsonPostBody();
+
+        response = exampleApiService.createANewPost(body);
+
+        assertThat(response.statusCode())
+                .as("status code")
+                .isEqualTo(HttpStatus.SC_CREATED);
+    }
+
+    private String createExampleJsonPostBody() {
+        JsonObject requestParams = new JsonObject();
+        requestParams.put("title", "foo");
+        requestParams.put("body", "bar");
+        requestParams.put("userId", "3091");
+        return requestParams.toJson();
     }
 }
